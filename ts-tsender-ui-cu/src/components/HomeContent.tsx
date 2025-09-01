d9a87eca6bc19a1eb134987d6b9d15192ce8708b
Kaloyan Stoichkov <kaloyanstoichkov9@gmail.com>
2025-09-01 12:03:59 +0300

    Updated Files
    Added gif before connecting to a wallet

--- ts-tsender-ui-cu/src/components/HomeContent.tsx
+++ ts-tsender-ui-cu/src/components/HomeContent.tsx
@@ -11,14 +11,22 @@ export default function HomeContent() {
     return (
         <main>
             {!isConnected ? (
-                <div className="flex items-center justify-center">
+                <div className="flex flex-col items-center justify-center space-y-4">
                     <h2 className="text-xl font-medium text-zinc-600">
                         Please connect a wallet...
                     </h2>
+                    <img
+                        src="/Shuu_GIF-downsized_large.gif"   // put your GIF in public/my-wallet.gif
+                        alt="Wallet animation"
+                        className="w-150 h-150 object-contain"
+                    />
                 </div>
             ) : (
                 <div className="flex items-center justify-center p-4 md:p-6 xl:p-8">
-                    <AirdropForm isUnsafeMode={isUnsafeMode} onModeChange={setIsUnsafeMode} />
+                    <AirdropForm
+                        isUnsafeMode={isUnsafeMode}
+                        onModeChange={setIsUnsafeMode}
+                    />
                 </div>
             )}
         </main>
